8176290ff7fd7dd2a37b54e2fa326b5368837803
James romanov <qahramonovjmashid1@gmail.com>
2025-04-02 22:59:37 +0500

    sone

--- client/html/another.js
+++ client/html/another.js
@@ -1,5 +1,5 @@
-const socket = io("http://localhost:3000");
-const userNamespace = io("http://localhost:3000/user", {
+const socket = io("http://localhost:80");
+const userNamespace = io("http://localhost:80/user", {
   auth: { token: "Test" },
 });
 
