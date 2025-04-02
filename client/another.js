049e46edf1fa8896ef1299d19806a07dadae83f9
James romanov <qahramonovjmashid1@gmail.com>
2025-04-02 17:30:03 +0500

    everthing is done

--- client/another.js
+++ client/another.js
@@ -1,4 +1,7 @@
 const socket = io("http://localhost:3000");
+const userNamespace = io("http://localhost:3000/user", {
+  auth: { token: "Test" },
+});
 
 let main = document.querySelector(".main");
 let messageInput = document.querySelector("#message");
@@ -6,21 +9,46 @@ let roomInput = document.querySelector("#room");
 let join = document.querySelector("#join");
 let send = document.querySelector("#send");
 let form = document.querySelector("#form");
-
+userNamespace.on("connect_error", (error) => {
+  displayMessage(error);
+});
 socket.on("connect", () => {
   displayMessage("you connected with this id" + "  " + socket.id);
 });
 socket.on("receive-message", (message) => {
   displayMessage(message);
+  main.appendChild(div);
 });
 form.addEventListener("submit", (e) => {
   let msg = messageInput.value;
-  socket.emit("send-message", msg); //to send a message
+  let room = roomInput.value;
+  if (msg == "") return;
+  displayMessage(msg, "right");
+  socket.emit("send-message", msg, room); //to send a message
   e.preventDefault();
   messageInput.value = "";
 });
-function displayMessage(message) {
+
+join.addEventListener("click", (e) => {
+  let room = roomInput.value;
+  socket.emit("join-room", room, (message) => {
+    displayMessage(message);
+  });
+});
+function displayMessage(message, pst = "left") {
   let div = document.createElement("div");
+  if (pst === "left") div.style.color = "blue";
+  else div.style.color = "black";
   div.textContent = message;
+  div.style.textAlign = pst;
   main.appendChild(div);
 }
+let count = 0;
+setInterval(() => {
+  socket.emit("count", ++count);
+}, 1000);
+document.addEventListener("keydown", (e) => {
+  if (e.target.matches === "input") return;
+  // if (e.key === "c") socket.connect();
+  // if (e.key === "d") socket.disconnect();
+});
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { instrument } = require("@socket.io/admin-ui");
+
 const express = require("express");
 const http = require("http");
 const app = express();
@@ -6,7 +8,8 @@ server = http.createServer(app);
 const io = require("socket.io");
 realServer = new io.Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://127.0.0.1:5500", "https://admin.socket.io"],
+    credentials: true,
   },
   // allowRequest: (req, callback) => {
   //   console.log(req.headers.host, req.headers.origin);
@@ -15,15 +18,55 @@ realServer = new io.Server(server, {
   // },
 });
 console.log("salom");
+
+const userNamespace = realServer.of("/user");
+userNamespace.on("connection", (socket) => {
+  console.log(socket.username);
+  console.log("connected user with " + socket.username);
+});
+userNamespace.use((socket, next) => {
+  console.log(socket.handshake.auth.token);
+  if (socket.handshake.auth.token) {
+    socket.username = getUserNameFromToken(socket.handshake.auth.token);
+    next();
+  } else {
+    next(new Error("Please enter token!"));
+  }
+}); // to set up diffirent middleware!
+
+function getUserNameFromToken(token) {
+  return token;
+}
 realServer.on("connection", (socket) => {
   console.log(socket.id);
   socket.on("disconnect", () => {
     console.log("user left the chat", socket.id);
   });
-  socket.on("send-message", (message) => {
-    console.log(message);
-    socket.emit("receive-message", message);
+  socket.on("send-message", (message, room) => {
+    if (room === "") {
+      socket.broadcast.emit("receive-message", message);
+      // socket.broadcast.emit("receive-message", message);
+    } else {
+      socket.to(room).emit("receive-message", message);
+    }
+
+    // console.log(message);
+    //sending the message to the only user that is connected!
+    //socket.broadcast.emit
+
+    // by default everysingle socket io has got their room
+    //for example they have room with their ids
+
+    //socket.join()  is for joining custom rooms
+  });
+  socket.on("join-room", (room, cb) => {
+    socket.join(room);
+    cb(`Joined ${room}`);
+  });
+  socket.on("count", (count) => {
+    console.log(count);
   });
 });
 
+instrument(realServer, { auth: false, mode: "development" });
 server.listen(3000);
